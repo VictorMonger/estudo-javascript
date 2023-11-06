735c4e2a4d4956f495899b0f1179cc62b98c78cc
Victor Monger <victormonger2@outlook.com>
2023-11-06 16:24:19 -0300

    exercises changes

--- exercisesResolution/disorderedArray.js
+++ exercisesResolution/disorderedArray.js
@@ -1,7 +1,10 @@
+const sortedArray = (unorderedArray) => {
+  const newArray = unorderedArray.sort((a, b) => {
+    return a - b;
+  });
+  return newArray;
+}
+
 const array = [53, 623, 45, 67, 78, 23, 243, 15, 69, 9, 37, 12, 45, 561, 7, 34, 1, 24, 22, 17, 5, 3];
 
-array.sort((a, b) => {
-  return a - b;
-});
-
-console.log(array);
+console.log(sortedArray(array));
