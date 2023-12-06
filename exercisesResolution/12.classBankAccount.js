1b236b4f0fe22fc44027a811ec1c4f3af787185b
Victor Monger <victormonger2@outlook.com>
2023-12-06 14:54:48 -0300

    changes

--- exercisesResolution/12.classBankAccount.js
+++ exercisesResolution/12.classBankAccount.js
@@ -1,5 +1,5 @@
 class BankAccount {
-  constructor (clientName, accountNumber, balance,){
+  constructor (clientName, accountNumber, balance){
     if (balance <= 0) {
       throw new Error ('The balance must be greater than zero to create a new account!');
     }
@@ -9,8 +9,8 @@ class BankAccount {
     this.bankStatement = [];
   }
 
-  getBankStatement(operation){
-    this.bankStatement.push(operation);
+  addBankStatement(type){
+    this.bankStatement.push(type);
   }
   
   setWithdrawal(value){
@@ -18,7 +18,7 @@ class BankAccount {
       throw new Error('Insufficient balance!');
     }
     this.balance = this.balance - value;
-    this.getBankStatement(`Withdrawal of ${value} - New balance: ${this.balance}`);
+    this.addBankStatement(`Withdrawal of ${value} - New balance: ${this.balance}`);
   }
   
   setDeposit(value){
@@ -26,15 +26,14 @@ class BankAccount {
       throw new Error('The deposit must be greater than zero');
     }
     this.balance = this.balance + value;
-    this.getBankStatement(`Deposit of ${value} - New balance: ${this.balance}`);
+    this.addBankStatement(`Deposit of ${value} - New balance: ${this.balance}`);
   }
 }
 
 const client1 = new BankAccount ('Victor Gabriel dos Santos Monger', '23443200', 1000);
 const client2 = new BankAccount ('Diogo Felipe Stefano Monger', '24554200', 30000);
 
-client1.setDeposit(600);
-client1.setWithdrawal(300);
+client1.setWithdrawal(1000);
 
 client2.setDeposit(11000);
 client2.setWithdrawal(15000);
